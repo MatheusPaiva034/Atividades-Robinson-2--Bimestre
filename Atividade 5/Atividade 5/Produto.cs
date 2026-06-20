827ae20ed99885737818509388233939aef51ceb
MPB <matheuspaivab2013@gmail.com>
2026-06-20 19:48:06 -0300

    Add files via upload

---
+++ Atividade 5/Atividade 5/Produto.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Produto
+{
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+}
---
+++ Atividade 5/Atividade 5/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<Produto> produtos = new List<Produto>();
+
+        double total = 0;
+
+        for (int i = 1; i <= 4; i++)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine($"Produto {i}");
+
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+
+            produtos.Add(p);
+        }
+
+        Console.WriteLine("\nProdutos cadastrados:");
+
+        foreach (Produto p in produtos)
+        {
+            Console.WriteLine($"{p.Nome} - R$ {p.Preco:F2}");
+            total += p.Preco;
+        }
+
+        Console.WriteLine($"\nValor total: R$ {total:F2}");
+
+        Console.ReadKey();
+    }
+}
